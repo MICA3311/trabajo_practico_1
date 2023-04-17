1ac6a8265b7957ce431d713b12b21b8e8017b5a6
paivae <18567047+paivae@users.noreply.github.com>
2023-04-17 11:08:32 -0300

    texto matrix

--- asserts/js/functions.js
+++ asserts/js/functions.js
@@ -1,6 +1,8 @@
 let typerI = 0;
 let typerDOM = 'typerBox';
 let typerText= '';
+let typerTimerOut = null;
+let typerCallback = null
 const typerSpeed = 50;
 
 
@@ -8,10 +10,16 @@ const typer = () => {
     if(typerI < typerText.length){
         document.getElementById(typerDOM).innerHTML += typerText.charAt(typerI);
         typerI++;
-        setTimeout(typer,typerSpeed);
+        typerTimerOut = setTimeout(typer,typerSpeed);
+    }else{
+        if(typeof(typerCallback) == "function"){
+            typerCallback();
+        }
+        
     }
 }
 
 const typerReset = () => {
+    document.getElementById(typerDOM).innerHTML = ''
     typerI = 0;
 }
--- asserts/js/index.js
+++ asserts/js/index.js
@@ -1,4 +1,15 @@
-typerText = 'Hola Camila, como estas?'
+const step1 = ()=>{
+    typerCallback = step2;
+    typerText = 'Te voy a explicar porque estas aqui. Estas porque sabes algo, aunque lo que sabes no puedes explicarlo, pero lo percibes. Ha sido asi durante toda tu vida, ¿Sabes de lo que estoy hablando?'
+    typerReset();
+    typer()
+}
 
-typerReset();
-typer();
+const step2 = ()=>{
+    typerCallback = null;
+    typerText = 'Has de verla con tus propios ojos, ultima oportunidad, despues de esto no podras echarte atras, si tomas la pastilla azul fin de la historia, despertaras en tu cama y creeras lo que quieras creer, pero si tomar la roja despertaras en el pais de las maravillas, yo te enseñare hasta donde llegan las madrigueras de conejo.'
+    setTimeout(typerReset,2000);
+    setTimeout(typer,3000);
+}
+
+step1()
