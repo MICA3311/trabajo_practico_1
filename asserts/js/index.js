53057190e129ad13eb602d2f41ba1b28aa4b2ea2
paivae <18567047+paivae@users.noreply.github.com>
2023-04-17 11:49:57 -0300

    intro

--- asserts/js/index.js
+++ asserts/js/index.js
@@ -7,9 +7,14 @@ const step1 = ()=>{
 
 const step2 = ()=>{
     typerCallback = null;
-    typerText = 'Has de verla con tus propios ojos, ultima oportunidad, despues de esto no podras echarte atras, si tomas la pastilla azul fin de la historia, despertaras en tu cama y creeras lo que quieras creer, pero si tomar la roja despertaras en el pais de las maravillas, yo te enseñare hasta donde llegan las madrigueras de conejo.'
+    typerText = 'Has de verla con tus propios ojos, ultima oportunidad, despues de esto no podras echarte atras, si tomas la pastilla azul fin de la historia, despertaras en tu cama y creeras lo que quieras creer, pero si tomar la roja despertaras en el pais de las maravillas, yo te enseñare hasta donde llega el agujero del conejo.'
     setTimeout(typerReset,2000);
     setTimeout(typer,3000);
 }
 
-step1()
+
+
+document.getElementById("scene-01").addEventListener("click",()=>{
+    document.getElementById("scene-01").classList.add('d-none');
+    document.getElementById("scene-02").classList.remove('d-none');
+})
